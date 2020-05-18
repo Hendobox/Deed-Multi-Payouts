6d2fce44bbb8fecea62c38780bb8bdbeb60e0a20
Hendobox <henryonyebuchi53@gmail.com>
2020-05-18 15:22:59 +0100

    initial commit

---
+++ contracts/DeedMultiPayouts.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.0;
+
+contract DeedMultiPayouts {
+    address public lawyer;  //lawyer's address. Should be the deployer
+    address payable public beneficiary;     //beneficiary's address
+    uint public earliest;       //earliest time before payments can be made
+    uint public amount;     //amount to be withdrawn
+    uint constant public PAYOUTS = 10;      //maximum number of times to make withdrawal
+    uint constant public INTERVAL = 10;     //time interva before next withdrawal
+    uint public paidPayouts;        //number of eligible withdrawals made
+    
+    //constructor function passes the various addresses and earliest withdrawal time on deployment
+    constructor(address _lawyer, address payable _beneficiary, uint fromNow) payable public {
+        lawyer = _lawyer;
+        beneficiary = _beneficiary;
+        earliest = now + fromNow;
+        amount = msg.value / PAYOUTS;
+    }
+    
+    //function for withdrawal of funds
+    function withdraw() public {
+        //beneficiary must be the caller
+        require(msg.sender == beneficiary, 'only beneficiary can call this');
+        //cannot make call before thestipulated time
+        require(now >= earliest, 'too early to make call');
+        //must not withdraw more than required times
+        require(paidPayouts < PAYOUTS, 'no payouts left');
+        //eligible payouts in time intervals
+        uint eligiblePayouts = (now - earliest) / INTERVAL;
+        //calculates the due number of payouts at a given time
+        uint duePayouts = eligiblePayouts - paidPayouts;
+        duePayouts = duePayouts + paidPayouts > PAYOUTS ? PAYOUTS - paidPayouts: duePayouts;
+        paidPayouts += duePayouts;
+        //transfers funds from smart contract to beneficiary
+        beneficiary.transfer(duePayouts * amount);
+    }
+}
+
