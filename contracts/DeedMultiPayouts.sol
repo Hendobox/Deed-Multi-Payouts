26f083e54937f1f9ffef71a972c00cc9998cdbf7
raneet <raneetdebnath10@gmail.com>
2020-11-05 19:48:06 +0530

    used safemath for preventing over/underflows and 'call' instead of 'transfer' (EIP 1884)

--- contracts/DeedMultiPayouts.sol
+++ contracts/DeedMultiPayouts.sol
@@ -1,6 +1,12 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.0;
 
-contract DeedMultiPayouts {
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract DeedMultiPayouts is ReentrancyGuard {
+    using SafeMath for uint256;
+    
     address public lawyer;  //lawyer's address. Should be the deployer
     address payable public beneficiary;     //beneficiary's address
     uint public earliest;       //earliest time before payments can be made
@@ -13,12 +19,12 @@ contract DeedMultiPayouts {
     constructor(address _lawyer, address payable _beneficiary, uint fromNow) payable public {
         lawyer = _lawyer;
         beneficiary = _beneficiary;
-        earliest = now + fromNow;
-        amount = msg.value / PAYOUTS;
+        earliest = now.add(fromNow) ;
+        amount = msg.value.div(PAYOUTS) ;
     }
     
     //function for withdrawal of funds
-    function withdraw() public {
+    function withdraw() public nonReentrant{
         //beneficiary must be the caller
         require(msg.sender == beneficiary, 'only beneficiary can call this');
         //cannot make call before thestipulated time
@@ -26,13 +32,15 @@ contract DeedMultiPayouts {
         //must not withdraw more than required times
         require(paidPayouts < PAYOUTS, 'no payouts left');
         //eligible payouts in time intervals
-        uint eligiblePayouts = (now - earliest) / INTERVAL;
+        uint eligiblePayouts = now.sub(earliest).div(INTERVAL);
         //calculates the due number of payouts at a given time
-        uint duePayouts = eligiblePayouts - paidPayouts;
-        duePayouts = duePayouts + paidPayouts > PAYOUTS ? PAYOUTS - paidPayouts: duePayouts;
-        paidPayouts += duePayouts;
+        uint duePayouts = eligiblePayouts.sub(paidPayouts) ;
+        duePayouts = duePayouts.add(paidPayouts)  > PAYOUTS ? PAYOUTS.sub(paidPayouts) : duePayouts;
+        paidPayouts = paidPayouts.add(duePayouts);
         //transfers funds from smart contract to beneficiary
-        beneficiary.transfer(duePayouts * amount);
+        (bool success,) = beneficiary.call.value(duePayouts * amount)("");
+        require(success, "Transfer failed");
+       // beneficiary.transfer(duePayouts * amount);
     }
 }
 
